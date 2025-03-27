d3fc672ed05e9e7cf72efb4c04ec7eaf0b6b40c7
Sarvesh Agarwal <Asarvesh38@gmail.com>
2025-03-27 17:19:22 +0530

    Update sql_convert.py

--- sql_convert.py
+++ sql_convert.py
@@ -2,11 +2,11 @@ import pandas as pd
 import sqlite3
 
 # Load CSV into a DataFrame
-csv_file_path = r"C:\Users\Asarv\Downloads\customers-100.csv"
+csv_file_path = r"C:\Users\customers-100.csv"
 df = pd.read_csv(csv_file_path)
 
 # Connect to SQLite database (creates a new database if not exists)
-db_path = r"C:\Users\Asarv\Downloads\customers-100.db"
+db_path = r"C:\Users\customers-100.db"
 conn = sqlite3.connect(db_path)
 
 # Save DataFrame to SQLite table
